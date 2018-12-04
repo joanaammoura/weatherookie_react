261694713e658fea71d550c410c668969313fd2d
joanamoura <joanaa.mmoura@users.noreply.github.com>
2018-12-04 18:37:52 +0000

    improvements?

--- src/App.js
+++ src/App.js
@@ -47,6 +47,10 @@ export default class AppLisbon extends Component {
     return days[date.getDay()] + " " + date.getHours() + ":" + minutes;
   }
 
+  weekDays(date) {
+    return date.getDay();
+  }
+
   refreshWeatherFromUrl(url) {
     axios.get(url).then(response => {
       this.setState({
@@ -57,7 +61,10 @@ export default class AppLisbon extends Component {
           precipitation: Math.round(response.data.main.humidity) + "%",
           temperature: Math.round(response.data.main.temp),
           time: this.friendlyDate(new Date()),
-          wind: Math.round(response.data.wind.speed) + "km/h"
+          weekDay: this.weekDays(new Date()),
+          wind: Math.round(response.data.wind.speed) + "km/h",
+          max: Math.round(response.data.main.temp_max),
+          min: Math.round(response.data.main.temp_min)
         }
       });
     });
@@ -71,24 +78,30 @@ export default class AppLisbon extends Component {
             <div className="row">
               <div className="col-md-6">
                 <div className="weather">
-                  <div className="weather-date">{this.state.time}</div>
-                  <div className="weather-time">{this.state.time}</div>
+                  <div className="weather-date">
+                    {this.state.conditions.time}
+                  </div>
+                  <div className="weather-time">
+                    {this.state.conditions.time}
+                  </div>
                   <div className="weather-today">
                     <WeatherIcon iconName={this.state.conditions.icon} />
                     <div className="weather-today__temperature">
-                      {this.condition.temperature} º
+                      {this.state.conditions.temperature} º
                     </div>
                   </div>
-                  <div className="weather-location">{this.condition.city}</div>
+                  <div className="weather-location">
+                    {this.state.conditions.city}
+                  </div>
                   <div className="weather-extras">
                     <div className="weather-precipitation">
-                      Precipitation: {this.condition.precipitation}%
+                      Precipitation: {this.state.conditions.precipitation}%
                     </div>
                     <div className="weather-humidity">
-                      Humidity: {this.condition.humidity}%
+                      Humidity: {this.state.conditions.humidity}%
                     </div>
                     <div className="weather-wind">
-                      Wind: {this.condition.wind}km/h
+                      Wind: {this.state.conditions.wind}km/h
                     </div>
                   </div>
                 </div>
@@ -99,153 +112,153 @@ export default class AppLisbon extends Component {
                 <div className="days">
                   <div className="day__block day__block--selected">
                     <div className="day__block-day">
-                      {this.condition.weekDays[0]}
+                      {this.state.conditions.weekDay[0]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[0]}º
+                        {this.state.conditions.max[0]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[0]}º
+                        {this.state.conditions.min[0]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block">
                     <div className="day__block-day">
-                      {this.condition.weekDays[1]}
+                      {this.state.conditions.weekDay[1]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[1]}º
+                        {this.state.conditions.max[1]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[1]}º
+                        {this.state.conditions.min[1]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block">
                     <div className="day__block-day">
-                      {this.condition.weekDays[2]}
+                      {this.state.conditions.weekDay[2]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[2]}º
+                        {this.state.conditions.max[2]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[2]}º
+                        {this.state.conditions.min[2]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block ">
                     <div className="day__block-day">
-                      {this.condition.weekDays[3]}
+                      {this.state.conditions.weekDay[3]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[3]}º
+                        {this.state.conditions.max[3]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[3]}º
+                        {this.state.conditions.min[3]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block ">
                     <div className="day__block-day">
-                      {this.condition.weekDays[4]}
+                      {this.state.conditions.weekDay[4]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[4]}º
+                        {this.state.conditions.max[4]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[4]}º
+                        {this.state.conditions.min[4]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block">
                     <div className="day__block-day">
-                      {this.condition.weekDays[5]}
+                      {this.state.conditions.weekDay[5]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[5]}º
+                        {this.state.conditions.max[5]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[5]}º
+                        {this.state.conditions.min[5]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block">
                     <div className="day__block-day">
-                      {this.condition.weekDays[6]}
+                      {this.state.conditions.weekDay[6]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[6]}º
+                        {this.state.conditions.max[6]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[6]}º
+                        {this.state.conditions.min[6]}º
                       </span>
                     </div>
                   </div>
                   <div className="day__block day__block--last">
                     <div className="day__block-day">
-                      {this.condition.weekDays[7]}
+                      {this.state.conditions.weekDay[7]}
                     </div>
                     <img
                       className="day__block-image"
                       alt="weather icon"
-                      src={this.condition.imgUrl}
+                      src={this.state.conditions.imgUrl}
                     />
                     <div className="day__block-temps">
                       <span className="day__block-temp day__block-temp--max">
-                        {this.condition.max[7]}º
+                        {this.state.conditions.max[7]}º
                       </span>
                       <span> </span>
                       <span className="day__block-temp day__block-temp--min">
-                        {this.condition.min[7]}º
+                        {this.state.conditions.min[7]}º
                       </span>
                     </div>
                   </div>
